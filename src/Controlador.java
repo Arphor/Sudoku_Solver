8458b76cdff463a21f3e9ca298d340fb8efe3eb6
Arphor <gabrielpaiva18@gmail.com>
2022-11-14 16:24:46 -0300

    Jarfile for PDFbox

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,15 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import Entidades.SudokuGraph;
+import Entidades.Vertex;
 
 public class Controlador {
     private SudokuGraph g;
-    private int colors;
+    private int totalColors;
+    private List<Integer> usedColors = new ArrayList<Integer>();
+
 
     public void readFile(String s){
 
     }
 
-    public void makeGraph(){
+    public void makeGraph(int array[][]){
+        try {
+            this.g = new SudokuGraph(array.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        this.totalColors = array.length;
+        for (int i = 0; i < array.length; i++){
+            for (int e = 0; e < array[i].length; e++){
+
+                if (!usedColors.contains(array[i][e])){
+                    usedColors.add(array[i][e]);
+                }
+
+                Vertex v = new Vertex(i, e);
+                v.setColor(array[i][e]);
+                g.setVertice(i, e, v);
+
+            }
+        }
     }
 
     public void colorGraph(){
--- src/Entidades/SudokuGraph.java
+++ src/Entidades/SudokuGraph.java
@@ -1,14 +1,26 @@
 package Entidades;
 import java.util.Arrays;
+import java.lang.Math;
+import java.lang.Exception;
 
 public class SudokuGraph {
     
     private Vertex[][] vertices;
     private int totalVertices = 0;
+    private int dim = 0;
+    private int regionsize = 0;
 
-    public SudokuGraph(int dim){
+    public SudokuGraph(int dim) throws Exception{
+        this.dim = dim;
         this.totalVertices = dim*dim;
         vertices = new Vertex[dim][dim];
+
+        double r = Math.sqrt(dim);
+        if ((r%1) != 0){
+            throw new Exception("Dimension error");
+        }
+        this.regionsize = (int) r;
+
         for (int i = 0; i < dim; i++){
             for (int e = 0; e < dim; e++){
                 vertices[i][e] = new Vertex(i, e);
@@ -30,6 +42,30 @@ public class SudokuGraph {
 
     public void setVertice(int x, int y, Vertex v){
         this.vertices[x][y] = v;
+    }
+
+    public void createEdgesRow(int x, int y){
+        for (int i = 0; i < dim; i++){
+            if (vertices[x][i] != vertices[x][y]){
+                vertices[x][y].addToAdjList(vertices[x][i]);
+            }
+        }
+    }
+
+    public void createEdgesColumn(int x, int y){
+        for (int i = 0; i < dim; i++){
+            if (vertices[i][y] != vertices[x][y]){
+                vertices[x][y].addToAdjList(vertices[i][y]);
+            }
+        }
+    }
+
+    public void createEdgesRegion(int x, int y){
+        for (int i = 0; i < dim; i++){
+            if (vertices[i][y] != vertices[x][y]){
+                vertices[x][y].addToAdjList(vertices[i][y]);
+            }
+        }
     }
 
     @Override
