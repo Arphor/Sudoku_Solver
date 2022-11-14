ab43e40f91e7a89573bf5852b91aa8ac6ec8a393
Arphor <gabrielpaiva18@gmail.com>
2022-11-14 16:59:48 -0300

    PDF reader

--- src/App.java
+++ src/App.java
@@ -1,9 +1,37 @@
+import java.io.File;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+
+        File file = new File("arquivos/Sudokus_Pattern_Easy.pdf");
+
+        try (PDDocument document = PDDocument.load( file )) {
+            System.out.print("oioioioi");
+
+            if (!document.isEncrypted()) {
+                System.out.print("oioioioi");
+			
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+            }
+
+
+
         System.out.println("Hello, World!");
+        }
+    }
     }
 }
