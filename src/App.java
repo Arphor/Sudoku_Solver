9a4642732c8ee8347af6b566059293e2d69664ff
Arphor <gabrielpaiva18@gmail.com>
2022-11-15 21:56:44 -0300

    Different Test

--- src/App.java
+++ src/App.java
@@ -12,22 +12,31 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int arr[][] = {
-            {0,0,0,0},
-            {0,0,0,0},
-            {0,0,0,0},
-            {0,0,0,0}
+            {7,2,3,0,0,0,1,5,9},
+            {6,0,0,3,0,2,0,0,8},
+            {8,0,0,0,1,0,0,0,2},
+            {0,7,0,6,5,4,0,2,0},
+            {0,0,4,2,0,7,3,0,0},
+            {0,5,0,9,3,1,0,4,0},
+            {5,0,0,0,7,0,0,0,3},
+            {4,0,0,1,0,3,0,0,6},
+            {9,3,2,0,0,0,7,1,4}
         };
         
+        final long startTime = System.currentTimeMillis();
         Controlador c = new Controlador();
         c.makeGraph(arr);
         c.createEdges();
         c.DSatur();
+
+        final long endTime = System.currentTimeMillis();
 
         System.out.println("Hello, World!");
 
         arr = c.print();
 
         System.out.println(Arrays.deepToString(arr));
+        System.out.println("Total execution time: " + (endTime - startTime) + " milliseconds");
 
     }
 }
